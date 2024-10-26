bdaaf5d9453f0d6f26a27680728bad47b1b62502
mejzerVitaliy <mejzervitalik@gmail.com>
2024-10-26 21:19:29 +0300

    update userSchema and add CORS policy

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose';
 import router from './router.js';
+import cors from 'cors'
 
 const PORT = 5000;
 const DB_URL = 'mongodb+srv://user:user@cluster0.nanvc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
 const app = express();
+
 app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://minimal-mind-v2.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
 app.use('/api', router)
 
 
--- models/userSchema.js
+++ models/userSchema.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const User = new mongoose.Schema({
     login: { type: String, required: true },
     password: { type: String, required: true },
+    id: { type: String, required: true },
     notes: {type: Array, required: true}
 })
 
